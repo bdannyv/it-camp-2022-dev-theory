bf011b6d9ed9352c449bc9d6fba0b6fe3f7b5774
danii <b.danny.v@gmail.com>
2022-08-10 20:57:25 +0300

    заготовка для теории

--- tests/test_sign_up.py
+++ tests/test_sign_up.py
@@ -1,69 +1,13 @@
-from db import User, UserGroup
-from models import SignUpRequest
-from fastapi.testclient import TestClient
-from app import app
-import pytest
-from unittest.mock import patch
-
-
-@pytest.fixture(scope='session')
-def user_group(session):
-    new = UserGroup(
-        name='manager'
-    )
-    session.add(new)
-    session.commit()
-    return new
-
-
-@pytest.fixture(scope='session')
-def input_data(user_group):
-    return SignUpRequest(
-        first_name="Steven",
-        last_name="Richardson",
-        email="testrich@me.com",
-        password="NotSoSimplePass",
-        group_id=user_group.id
-    )
-
-
 # Тест успешно зарегистрированного пользователя
 def test_signup_success(input_data, session):
-    client = TestClient(app)
-    with patch('app.session_factory') as patched:
-        patched.return_value = session
-        user = client.post('/users/signup', json=input_data.dict())
-
-        assert user.status_code == 200
-
-        db_user = session.query(User).filter_by(**user.json()).first()
-
-        assert db_user, 'User has not registered'
+    pass
 
 
 # Тест на попытку регистрации по существующему в базе email'у
 def test_signup_with_existing_email(input_data, session):
-
-    client = TestClient(app)
-    with patch('app.session_factory') as patched:
-        patched.return_value = session
-        user = client.post('/users/signup', json=input_data.dict())
-
-    assert user.status_code == 409
+    pass
 
 
 # Тес на неправильный тип данных при попытке регистрации (email=5)
 def test_validation_error():
-    client = TestClient(app)
-    user = client.post(
-        '/users/signup',
-        json={
-            "first_name": "Steven",
-            "last_name": "Richardson",
-            "email": "testrich3@me.com",
-            "password": "NotSoSimplePass",
-            "group_id": 'manager'
-        }
-    )
-
-    assert user.status_code == 422
+    pass
