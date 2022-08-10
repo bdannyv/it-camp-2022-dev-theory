965542a9ff2522fbff6ebb1eea3129129dce3936
danii <b.danny.v@gmail.com>
2022-08-11 01:13:25 +0300

    готовый сервис

---
+++ alembic/versions/17b5c4084bdc_initial_migration.py
@@ -0,0 +1,24 @@
+"""initial migration
+
+Revision ID: 17b5c4084bdc
+Revises: 
+Create Date: 2022-08-11 01:10:15.845611
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '17b5c4084bdc'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
